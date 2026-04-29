6dc375b88a3ad9e2626a4ac6ee811b9e2f3ec8d6
combro2k <github.t7k0x@obfuscation.work>
2026-04-30 00:39:09 +0200

    remove outside migrations

--- package/uninstall.sh
+++ package/uninstall.sh
@@ -5,12 +5,10 @@ PREFIX="${PREFIX:-/opt/birthday-reminders}"
 BINDIR="$PREFIX/bin"
 CONFDIR="$PREFIX/etc"
 DATADIR="$PREFIX/data"
-MIGRATIONSDIR="$PREFIX/migrations"
 STATICDIR="$PREFIX/static"
 
 # Remove installed files and directories
 rm -f "$BINDIR/birthday-reminders"
-rm -rf "$MIGRATIONSDIR"
 rm -rf "$STATICDIR"
 rm -rf "$DATADIR"
 
